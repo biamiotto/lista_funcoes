d1e805cf5f01dc6bf1dc3c01d78c4a839c06aa72
biamiotto <miotto.beatriz2008@gmail.com>
2025-05-23 21:15:30 -0300

    exercício01

--- exercicio01.js
+++ exercicio01.js
@@ -1,1 +1,19 @@
-kskdjjsjska
+function ehPrimo (numero) {
+    if (numero <= 1) {
+        return false;
+    }
+
+for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+        return false;
+    }
+}
+
+return true;
+
+}
+
+console.log(ehPrimo(2));
+console.log(ehPrimo(10));
+console.log(ehPrimo(5));
+console.log(ehPrimo(8));
