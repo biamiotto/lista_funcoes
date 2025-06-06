15dd99841e92b10dff1e49fdae4be883ce0926c9
biamiotto <miotto.beatriz2008@gmail.com>
2025-06-05 22:14:55 -0300

    exercicio07

---
+++ exercicio08.js
@@ -0,0 +1,13 @@
+function filtrarMaioresQue (arr, n) {
+    let resultado = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > n) {
+            resultado.push(arr[i]);
+        }
+    }
+
+    return resultado;
+}
+
+console.log(filtrarMaioresQue([100, 500, 70, 150, 200], 150));
